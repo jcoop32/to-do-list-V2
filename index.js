b7ed5123e03d3a562c5b1b9e923abbaf9269d666
jcoop32 <jcooper11@hawk.iit.edu>
2023-05-26 18:25:07 -0500

    layout started

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//to resolve prompt reference error
+const prompt = require("prompt-sync")();
+
 //things we need:
 /*
 if user wants to add, edit, or delete a new task
@@ -5,6 +8,66 @@ if add -> ask what they want to add -> add to list which has a number assigned t
 if edit -> ask for which number of task wanted to change -> prompt for new entry
 if delete -> ask for which number of task -> remove from list
 */
+let listLimit = 3;
+
 
 const listItems = [];
+
+//get user input for what to do
+function taskAtHand(){
+    console.log(`What you you want do to your To Do list?`);
+    console.log(`1. Add Task`);
+    console.log(`2. Edit Task`);
+    console.log(`3. Delete Task`);
+    let userChoice = prompt('');
+    userChoice = userChoice.toLowerCase();
+    if (userChoice === '1'){
+        addTask();
+        listLimit = listLimit - 1;
+    } else if (userChoice === '2'){
+        editTask();
+        listLimit = listLimit - 1;
+    } else if (userChoice === '3'){
+        deleteTask();
+        listLimit = listLimit - 1;
+
+    }
+}
+
+function addTask(){
+    console.log('you added a task');
+}
+function editTask(){
+    console.log('you added a task');
+}
+
+function deleteTask(){
+    console.log('you added a task');
+}
+
+
+
+
+//loop to ask if user wants to continue
+
+while (listLimit > 0){
+    taskAtHand();
+    let userLoop = prompt('Continue? (y/n)');
+    if (userLoop === 'y'){
+        taskAtHand();
+    } else if (userLoop === 'n'){
+        console.log("nice");
+        break;
+    }
+}
+
+
+
+
+
+
+
+
+
+
 
