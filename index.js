b77130f491df7fc87236a5d48f19f1b3b54adf59
jcoop32 <jcooper11@hawk.iit.edu>
2023-05-26 18:00:16 -0500

    description of functionality

---
+++ index.js
@@ -0,0 +1,10 @@
+//things we need:
+/*
+if user wants to add, edit, or delete a new task
+if add -> ask what they want to add -> add to list which has a number assigned to it 
+if edit -> ask for which number of task wanted to change -> prompt for new entry
+if delete -> ask for which number of task -> remove from list
+*/
+
+const listItems = [];
+
