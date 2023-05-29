80fbaf094f52088cc719287984d8997c36a7251b
jcoop32 <jcooper11@hawk.iit.edu>
2023-05-29 09:18:38 -0500

    more functionality

--- index.js
+++ index.js
@@ -7,92 +7,144 @@ if user wants to add, edit, or delete a new task
 if add -> ask what they want to add -> add to list which has a number assigned to it 
 if edit -> ask for which number of task wanted to change -> prompt for new entry
 if delete -> ask for which number of task -> remove from list
+list tasks
+exit program
+complete a task
+
+
+display a list of menu options
+user will select an option
+add- run addTask() -> return to menu
+edit - run editTask() -> return to menu
+delete - run deleteTask() -> return to menu
+complete - run completeTask() - return to menu
+exit - break loop
+read the input and call function 
+return to menu
+
+
+completeTask() -> if isDone = false -> mark complete
+
 */
-let listLimit = 4;
 
+// const itemS = {
+//     description: 'wash dishes',
+//     isDone: false,
+//     isDeleted: false,
+// }
 
-const listItems = [];
+let exitProgram = false;
+
+ const listItems = [
+    {
+    description: 'wash dishes',
+    isDone: false,
+    isDeleted: false, 
+    }, 
+    {
+    description: 'sweep floor',
+    isDone: false,
+    isDeleted: false,
+    }
+];
 
 //get user input for what to do
-function taskAtHand(){
-    console.log(`What you you want do to your To Do list?`);
-    console.log(`1. Add Task`);
-    console.log(`2. Edit Task`);
-    console.log(`3. Delete Task`);
-    let userChoice = prompt('');
-    userChoice = userChoice.toLowerCase();
-    if (userChoice === '1'){
+function menu(){
+    let userChoice = prompt(`Command: (a)dd, (e)dit, (d)elete, (c)omplete, (l)ist, (L)ist All, e(x)it: `);
+    if (userChoice === 'a'){
         addTask();
-        listLimit = listLimit - 1;
-    } else if (userChoice === '2'){
+        displayListItems();
+    } else if (userChoice === 'e'){
         editTask();
-    } else if (userChoice === '3'){
+        displayListItems();
+    } else if (userChoice === 'd'){
         deleteTask();
-        listLimit = listLimit + 1;
-
+        displayListItems();
+    } else if (userChoice === 'c'){
+        completeTask();
+    } else if (userChoice === 'l'){
+        displayListItems();
+    } else if (userChoice === 'L'){
+        listAll();
+    } else if (userChoice === 'x'){
+        listAll();
+        exitProgram = true;
+    } else{
+        console.log("command not found");
     }
 }
 
 function addTask(){
     let item = prompt("Task to add: ");
-    listItems.push(item);
-    console.log(`----------------------`)
-    displayListItems();
-    if (listLimit > 1){
-        console.log('you added a task');
-    }
-    console.log(`----------------------`)
+    const toDo = {
+        description: item,
+        isDone: false,
+        isDeleted: false
+    };
+    listItems.push(toDo);
+    // console.log('you added a task');
 }
+
 function editTask(){
+    
     let item = prompt("Enter number of task to edit: ");
     item = parseInt(item);
     
-    console.log(`----------------------`)
-    let newTask = prompt(`${item}: `);
-    listItems[item - 1] = newTask;
-   
-    console.log('you edited a task');
-    console.log(`----------------------`)
+    if (!listItems[item - 1].isDone){ 
+        let newTask = prompt(`${item}: `);
+        listItems[item - 1].description = newTask;   
+    } else{ //if isDone = true -> task already completed
+        console.log(`Task already completed.`)
+    }
 }
 
 function deleteTask(){
     let item = prompt("Enter number of task to delete: ");
     item = parseInt(item);
-    console.log(`----------------------`)
-    listItems.splice(item, 1);
-    console.log(`----------------------`)
-    console.log(`you deleted '${listItems[item - 1]}' from your list.`);
-    console.log(`----------------------`)
+    listItems.splice((item - 1), 1);
+}
+
+function completeTask(){
+    let item = prompt("Enter number of task to complete: ");
+    item = parseInt(item);
+    listItems[item - 1].isDone = true;
+    displayListItems();
+
 }
 
 //loop to ask if user wants to continue
-
-while (listLimit > 0){
-    taskAtHand();
-    let userLoop = prompt('Continue? (y/n)');
-    if (userLoop === 'y'){
-        console.log(`List: ${listItems}`);
-        //taskAtHand();
-    } else if (userLoop === 'n'){
-        console.log(`To Do List:`);
-        console.log(`------------------`)
-        displayListItems();
-        console.log(`\n------------------`)
-        break;
-    }
+while (!exitProgram){
+    menu();
 }
 
 function displayListItems(){
+    console.log(`\n\n----------------------`)
+    console.log(`Todo List`)
+
+    console.log(`----------------------`)
     for (let i = 0; i < listItems.length; i++){
-        console.log(`${i + 1}: ${listItems[i]}`);
+        if(listItems[i].isDone || listItems[i].isDeleted){
+            continue;
+        }
+        console.log(`${i + 1}: ${listItems[i].description}`);
+        //console.log(`${i + 1}: ${listItems[i].description}  (${listItems[i].isDone})`);
     }
+    console.log(`----------------------\n\n`)
 }
 
+function completedList(){
+    console.log(`Completed Tasks`);
+    console.log(`----------------------`);
+    for (let i = 0; i < listItems.length; i++){
+       if(listItems[i].isDone && !listItems[i].isDeleted){
+        console.log(`${i + 1}: ${listItems[i].description}`);
+        //console.log(`${i + 1}: ${listItems[i].description} (${listItems[i].isDone})`);
+       }
+    }
+    console.log(`----------------------\n\n`)
+}
 
-
-
-
-
-
-
-
+function listAll(){
+    displayListItems();
+    completedList();
+}
--- test.js
+++ test.js
@@ -7,102 +7,143 @@ if user wants to add, edit, or delete a new task
 if add -> ask what they want to add -> add to list which has a number assigned to it 
 if edit -> ask for which number of task wanted to change -> prompt for new entry
 if delete -> ask for which number of task -> remove from list
+list tasks
+exit program
+complete a task
+
+
+display a list of menu options
+user will select an option
+add- run addTask() -> return to menu
+edit - run editTask() -> return to menu
+delete - run deleteTask() -> return to menu
+complete - run completeTask() - return to menu
+exit - break loop
+read the input and call function 
+return to menu
+
+
+completeTask() -> if isDone = false -> mark complete
+
 */
-let listLimit = 0;
-const listItems = [];
 
-const totalList = {
-    listItems,
-}
+// const itemS = {
+//     description: 'wash dishes',
+//     isDone: false,
+//     isDeleted: false,
+// }
 
+let exitProgram = false;
+
+ const listItems = [
+    {
+    description: 'wash dishes',
+    isDone: false,
+    isDeleted: false, 
+    }, 
+    {
+    description: 'sweep floor',
+    isDone: false,
+    isDeleted: false,
+    }
+];
 
 //get user input for what to do
-function taskAtHand(){
-    let userChoice = prompt('');
-    if (userChoice === '1'){
+function menu(){
+    let userChoice = prompt(`Command: (a)dd, (e)dit, (d)elete, (c)omplete, (l)ist, (L)ist All, e(x)it: `);
+    
+    if (userChoice === 'a'){
         addTask();
-        listLimit = listLimit + 1;
-    } else if (userChoice === '2'){
+        displayListItems();
+    } else if (userChoice === 'e'){
         editTask();
-    } else if (userChoice === '3'){
+        displayListItems();
+    } else if (userChoice === 'd'){
         deleteTask();
-        listLimit = listLimit - 1;
+        displayListItems();
+    } else if (userChoice === 'c'){
+        completeTask();
+    } else if (userChoice === 'l'){
+        displayListItems();
+    } else if (userChoice === 'L'){
+        listAll();
+    } else if (userChoice === 'x'){
+        exitProgram = true;
+    } else{
+        console.log("command not found");
     }
 }
 
 function addTask(){
     let item = prompt("Task to add: ");
-    listItems.push(item); //add the users input into array
-    console.log(`----------------------`)
-    displayListItems();
+    const toDo = {
+        description: item,
+        isDone: false,
+        isDeleted: false
+    };
+    listItems.push(toDo);
+    // console.log('you added a task');
 }
 
 function editTask(){
-    let item = prompt("Enter number of task to edit: "); //ask for which task to change
-    item = parseInt(item); //turning string into int
-    console.log(`----------------------`)
-    let newTask = prompt(`${item}: `); 
-    listItems[item - 1] = newTask; //setting input to change the selected task 
-    console.log('you edited a task');
-    console.log(`----------------------`)
-    displayListItems();
+    
+    let item = prompt("Enter number of task to edit: ");
+    item = parseInt(item);
+    
+    if (!listItems[item - 1].isDone){ 
+        let newTask = prompt(`${item}: `);
+        listItems[item - 1].description = newTask;   
+    } else{ //if isDone = true -> task already completed
+        console.log(`Task already completed.`)
+    }
 }
 
 function deleteTask(){
     let item = prompt("Enter number of task to delete: ");
     item = parseInt(item);
-    console.log(`----------------------`)
-    let confirmation = prompt(`Are you sure you want to delete '${listItems[item - 1]}' from your list? (y/n)`);
-    confirmation = confirmation.toLowerCase();
-    if (confirmation === 'y'){  
-        console.log(`----------------------`)
-        listItems.splice(item, 1);//deletes selected item from array splice(start -> users selected option, delete count -> 1)
-        console.log(`you deleted '${listItems[item - 1]}' from your list.`);
-        console.log(`----------------------`);
-    }
-    
+    listItems.splice((item - 1), 1);
+}
+
+function completeTask(){
+    let item = prompt("Enter number of task to complete: ");
+    item = parseInt(item);
+    listItems[item - 1].isDone = true;
+    displayListItems();
+
 }
 
 //loop to ask if user wants to continue
-
-while (listLimit < 20){
-    console.log(totalList);
+while (!exitProgram){
     menu();
-    let userExit = prompt('Press any key to continue or Exit(e)'); //to see if user wants to exit
-    if (userExit === 'e'){
-        displayListItems();
-        break;
-    } else if (listItems > 3  || listItems % 2 !== 0){
-        let userNewList = prompt(`Do you want to create a new list? (y/n)`);
-        userNewList = userNewList.toLowerCase();
-            if (userNewList === `y`){
-                for (let i = 0; i < 3; i++){
-                    totalList.list`${i}`;
-                    console.log(totalList.listItems`${i}`);
-                }
-            } else {
-                continue;
-            }
-    }
-     else {
-        displayListItems();
-        continue;
-    }
-    
-    
-    
 }
 
 function displayListItems(){
-    for (let i = 0; i < totalList.length; i++){
-        console.log(`${i + 1}: ${listItems.list}`);
+    console.log(`\n\n----------------------`)
+    console.log(`Todo List`)
+
+    console.log(`----------------------`)
+    for (let i = 0; i < listItems.length; i++){
+        if(listItems[i].isDone || listItems[i].isDeleted){
+            continue;
+        }
+        //console.log(`${i + 1}: ${listItems[i].description}  (${listItems[i].isDone})`);
     }
+    console.log(`----------------------\n\n`)
 }
 
-function menu(){
-    console.log(`What you you want do to your To Do list?`);
-    console.log(`1. Add Task`);
-    console.log(`2. Edit Task`);
-    console.log(`3. Delete Task`);
-    taskAtHand();
+function completedList(){
+    console.log(`Completed Tasks`);
+    console.log(`----------------------`);
+    for (let i = 0; i < listItems.length; i++){
+       if(listItems[i].isDone && !listItems[i].isDeleted){
+        console.log(`${i + 1}: ${listItems[i].description}`);
+        //console.log(`${i + 1}: ${listItems[i].description} (${listItems[i].isDone})`);
+       }
+    }
+    console.log(`----------------------\n\n`)
+}
+
+function listAll(){
+    displayListItems();
+    completedList();
 }
